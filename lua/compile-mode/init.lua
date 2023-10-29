9b36f921361694c6968c854b66f5651ef41d493a
ej-shafran <ejshafran@hilma.tech>
2023-10-29 21:05:22 +0200

    Temporary - start using table for parsing

--- lua/compile-mode/init.lua
+++ lua/compile-mode/init.lua
@@ -2,6 +2,7 @@
 ---@alias SMods { vertical: boolean?, silent: boolean?, split: SplitModifier? }
 ---@alias CommandParam { args: string?, smods: SMods? }
 ---@alias Config { no_baleia_support: boolean?, default_command: string?, time_format: string?, baleia_opts: table?, buffer_name: string? }
+---@alias IntByInt { [1]: integer, [2]: integer }
 
 local a = require("plenary.async")
 ---@diagnostic disable-next-line: undefined-field
@@ -19,45 +20,112 @@ M.config = {}
 
 ---TODO: document
 ---(REGEXP FILE [LINE COLUMN TYPE HYPERLINK HIGHLIGHT...])
----@type table<string, { [1]: string, [2]: integer, [3]: integer|nil, [4]: integer|nil, [5]: nil|0|1|2 }>
+---@type table<string, { [1]: string, [2]: integer, [3]: integer|IntByInt|nil, [4]: integer|IntByInt|nil, [5]: nil|0|1|2 }>
 local error_regexp_table = {
 	-- TODO: use the actual alist from Emacs
-	-- TODO: should be the `gnu` key
-	default = { "^\\([^:]\\+\\):\\(\\d\\+\\):\\(\\d\\+\\)", 1, 2, 3, nil },
+	gnu = {
+		"^\\%([[:alpha:]][-[:alnum:].]\\+: \\?\\|[ 	]\\%(in \\| from\\)\\)\\?\\(\\%([0-9]*[^0-9\\n]\\)\\%([^\\n :]\\| [^-/\\n]\\|:[^ \\n]\\)\\{-}\\)\\%(: \\?\\)\\([0-9]\\+\\)\\%(-\\([0-9]\\+\\)\\%(\\.\\([0-9]\\+\\)\\)\\?\\|[.:]\\([0-9]\\+\\)\\%(-\\%(\\([0-9]\\+\\)\\.\\)\\([0-9]\\+\\)\\)\\?\\)\\?:\\%( *\\(\\%(FutureWarning\\|RuntimeWarning\\|W\\%(arning\\)\\|warning\\)\\)\\| *\\([Ii]nfo\\%(\\>\\|formationa\\?l\\?\\)\\|I:\\|\\[ skipping .\\+ ]\\|instantiated from\\|required from\\|[Nn]ote\\)\\| *\\%([Ee]rror\\)\\|\\%([0-9]\\?\\)\\%([^0-9\\n]\\|$\\)\\|[0-9][0-9][0-9]\\)",
+		1,
+		{ 2, 4 },
+		{ 3, 5 },
+		-- TODO: implement this
+		nil,
+	},
 }
+
+---Parses error syntax from a given line.
+---@param line string the line to parse
+---@return boolean ok whether there is an error here
+---@return nil|string filename the filename for the error
+---@return nil|integer r the row of the error
+---@return nil|integer c the column of the error
+local function parse_error(line)
+	local matcher = error_regexp_table["gnu"]
+	local regex = matcher[1]
+	local result = vim.fn.matchlist(line, regex)
+	if not result or #result == 0 or result[1] == "" then
+		return false, nil, nil, nil
+	end
+
+	local file_index = matcher[2] + 1
+	local filename = result[file_index]
+
+	local r
+	local r_indices = matcher[3]
+	if type(r_indices) == "number" then
+		if result[r_indices + 1] and result[r_indices + 1] ~= "" then
+			r = result[r_indices + 1]
+		else
+			return true, filename, 1, 1
+		end
+	elseif type(r_indices) == "table" then
+		local first = r_indices[1] + 1
+		local second = r_indices[2] + 1
+
+		if result[first] and result[first] ~= "" then
+			r = tonumber(result[first])
+		elseif result[second] and result[second] ~= "" then
+			r = tonumber(result[second])
+		else
+			return true, filename, 1, 1
+		end
+	else
+		return true, filename, 1, 1
+	end
+
+	local c
+	local c_indices = matcher[4]
+	if type(c_indices) == "number" then
+		if result[c_indices + 1] and result[c_indices + 1] ~= "" then
+			c = result[c_indices + 1]
+		else
+			return true, filename, r, 1
+		end
+	elseif type(c_indices) == "table" then
+		local first = c_indices[1] + 1
+		local second = c_indices[2] + 1
+
+		if result[first] and result[first] ~= "" then
+			c = tonumber(result[first])
+		elseif result[second] and result[second] ~= "" then
+			c = tonumber(result[second])
+		else
+			return true, filename, r, 1
+		end
+	else
+		return true, filename, r, 1
+	end
+
+	return true, filename, r, c
+end
 
 ---@param line string
 local function color_statement_prefix(line)
+	local ok, filename, r, c = parse_error(line)
+
 	-- TODO: allow customizing these
 	local normal = "\x1b[0m"
 	local red_bold_underline = "\x1b[31;1;4m"
-	local green_underline = "\x1b[32;4m"
-	local normal_underline = "\x1b[0;4m"
+	-- local green_underline = "\x1b[32;4m"
+	-- local normal_underline = "\x1b[0;4m"
 	local blue = "\x1b[34m"
 
-	-- TODO: check the type of the command, and use the appropriate matcher
-	local matcher = error_regexp_table["default"]
-	local regex = matcher[1]
-
-	if vim.fn.match(line, regex) then
-		return vim.fn.substitute(
-			line,
-			regex,
-			string.format(
-				-- TODO: use a HIGHLIGHT option here
-				"%s\\1%s:%s\\2%s:\\3%s",
-				red_bold_underline,
-				normal_underline,
-				green_underline,
-				normal_underline,
-				normal
-			),
-			""
-		) --[[@as string]]
-	else
+	if not ok or not filename or not r or not c then
 		-- TODO: place this somewhere else
+		-- Also, maybe do this more properly
 		return vim.fn.substitute(line, "^\\([^: \\t]\\+\\):", blue .. "\\1" .. normal .. ":", "") --[[@as string]]
 	end
+
+	-- TODO: check the type of the command, and use the appropriate matcher
+	-- local matcher = error_regexp_table["gnu"]
+	-- local regex = matcher[1]
+
+	local start, end_ = string.find(line, filename)
+	if start ~= nil and end_ ~= nil then
+		line = line:sub(1, start - 1) .. red_bold_underline .. filename .. normal .. line:sub(end_ + 1, -1)
+	end
+
+	return line
 end
 
 ---@param bufnr integer
@@ -142,38 +210,6 @@ end
 local function default_dir()
 	local cwd = vim.fn.getcwd() --[[@as string]]
 	return cwd:gsub("^" .. vim.env.HOME, "~")
-end
-
----Parses error syntax from a given line.
----@param line string the line to parse
----@return boolean ok whether there is an error here
----@return nil|string filename the filename for the error
----@return nil|integer r the row of the error
----@return nil|integer c the column of the error
-local function parse_error(line)
-	local matcher = error_regexp_table["default"]
-	local regex = matcher[1]
-	local result = vim.fn.matchlist(line, regex)
-	if not result or result[1] == "" then
-		return false, nil, nil, nil
-	end
-
-	local file_index = matcher[2] + 1
-	local filename = result[file_index]
-
-	if not matcher[3] then
-		return true, filename, 1, 1
-	end
-	local r_index = matcher[3] + 1
-	local r = tonumber(result[r_index])
-
-	if not matcher[4] then
-		return true, filename, r, 1
-	end
-	local c_index = matcher[4] + 1
-	local c = tonumber(result[c_index])
-
-	return true, filename, r, c
 end
 
 ---Go to the error on the current line
