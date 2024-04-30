b37ea51a2bf9f356a555f0a43dd6555c6accf123
Ivan <uuuulopta@gmail.com>
2024-04-30 19:44:51 +0200

    Fix default command name causing crash

--- lua/compile-mode/init.lua
+++ lua/compile-mode/init.lua
@@ -15,7 +15,7 @@ local current_error = 0
 local prev_dir = nil
 ---@type Config
 M.config = {
-	buffer_name = "*compilation*",
+	buffer_name = "compilation",
 	default_command = "make -k",
 	error_highlights = colors.default_highlights,
 	time_format = "%a %b %e %H:%M:%S",
