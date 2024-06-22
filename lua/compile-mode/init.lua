17bc8c55799b9b20dfc86492ea2e30b6dd6062b1
ej-shafran <ejshafran@hilma.tech>
2024-06-22 18:12:54 +0300

    Make `ask_about_save` & `ask_to_interrupt` default

--- lua/compile-mode/init.lua
+++ lua/compile-mode/init.lua
@@ -286,6 +286,8 @@ M.config = {
 	buffer_name = "*compilation*",
 	default_command = "make -k",
 	time_format = "%a %b %e %H:%M:%S",
+	ask_about_save = true,
+	ask_to_interrupt = true,
 }
 
 M.level = errors.level
