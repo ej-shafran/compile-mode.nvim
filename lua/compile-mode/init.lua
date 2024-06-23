d96d30c0c70e2c901a64b7c2c5854a9e5c5b018d
ej-shafran <ejshafran@hilma.tech>
2024-06-23 04:04:31 +0300

    Modify type hint

--- lua/compile-mode/init.lua
+++ lua/compile-mode/init.lua
@@ -1,5 +1,5 @@
 ---@alias SplitModifier "aboveleft"|"belowright"|"topleft"|"botright"|""
----@alias SMods { vertical: boolean?, silent: boolean?, split: SplitModifier?, hide: boolean? }
+---@alias SMods { vertical: boolean?, silent: boolean?, split: SplitModifier?, hide: boolean?, tab: integer? }
 ---@alias CommandParam { args: string?, smods: SMods?, bang: boolean?, count: integer }
 ---@alias Config { default_command: string?, time_format: string?, buffer_name: string?, error_regexp_table: ErrorRegexpTable?, debug: boolean?, error_ignore_file_list: string[]?, hidden_output: (string|string[])?, recompile_no_fail: boolean?, same_window_errors: boolean?, auto_jump_to_first_error: boolean?, ask_about_save: boolean?, environment: table<string, string>?, clear_environment: boolean?, ask_to_interrupt: boolean? }
 
