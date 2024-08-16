85caf3a077fb5a85d92791598952d7e752ccf2f5
ejshafran <ejshafran@hilma.tech>
2024-08-17 02:04:24 +0300

    Update bug report template

--- .github/ISSUE_TEMPLATE/bug_report.md
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -1,10 +1,9 @@
 ---
 name: Bug report
 about: Create a report to help us improve
-title: ''
+title: ""
 labels: bug
-assignees: ''
-
+assignees: "ej-shafran"
 ---
 
 **Describe the bug**
@@ -12,9 +11,12 @@ A clear and concise description of what the bug is.
 
 **To Reproduce**
 Steps to reproduce the behavior:
+
 1. Open a buffer
 2. Run ':SomeCommand...'
 3. See error
+
+It might be helpful to create a reproducible config file for someone trying to see the bug - you can use [this file](https://github.com/ej-shafran/compile-mode.nvim/raw/main/spec/configs/repro.lua) as a starting off point.
 
 **Expected behavior**
 A clear and concise description of what you expected to happen.
@@ -29,8 +31,8 @@ Include the result of `nvim -v`:
 PASTE HERE
 ```
 
-**compile-mode.nvim Version**
-Specify what version of `compile-mode.nvim` you are using.
+**compile-mode.nvim Version+Config**
+Specify what version of `compile-mode.nvim` you are using. Additionally, include your configuration for the plugin.
 
 **Additional context**
 Add any other context about the problem here, including any errors mentioned in `:checkhealth compile-mode`.
---
+++ spec/configs/repro.lua
@@ -0,0 +1,53 @@
+local root = vim.fn.fnamemodify("./.repro", ":p")
+
+-- set stdpaths to use .repro
+for _, name in ipairs({ "config", "data", "state", "cache" }) do
+	vim.env[("XDG_%s_HOME"):format(name:upper())] = root .. "/" .. name
+end
+
+-- bootstrap lazy
+local lazypath = root .. "/plugins/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--single-branch",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+-- install plugins
+local compile_mode_lazy_spec = {
+	"ej-shafran/compile-mode.nvim",
+	branch = "latest",
+	-- or a specific version:
+	-- tag = "v4.0.0"
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		-- if you want to enable coloring of ANSI escape codes in
+		-- compilation output, add:
+		-- { "m00qek/baleia.nvim", tag = "v1.3.0" },
+	},
+	config = function()
+		--- add configuration options here
+		---@type CompileModeOpts
+		vim.g.compile_mode = {
+			-- to add ANSI escape code support, add:
+			-- baleia_setup = true,
+		}
+	end,
+}
+local plugins = {
+	"folke/tokyonight.nvim",
+	compile_mode_lazy_spec,
+}
+require("lazy").setup(plugins, {
+	root = root .. "/plugins",
+})
+
+-- additional
+vim.opt.termguicolors = true
+vim.cmd([[colorscheme tokyonight]])
