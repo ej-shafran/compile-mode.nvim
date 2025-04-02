b76a96442092ac387ce81fa62f28745df47346c4
ejshafran <ejshafran@hilma.tech>
2025-04-02 14:39:01 +0300

    Fix test in Windows

--- spec/bang_expansion_spec.lua
+++ spec/bang_expansion_spec.lua
@@ -3,6 +3,10 @@ local assert = require("luassert")
 
 local filename = "somefile"
 
+local function print_command(value)
+	return "echo " .. (vim.o.shell:match("cmd.exe$") and value or vim.fn.shellescape(value))
+end
+
 describe("bang_expansion", function()
 	before_each(function()
 		helpers.setup_tests({ bang_expansion = true })
@@ -10,16 +14,16 @@ describe("bang_expansion", function()
 
 	it("should expand % in a command", function()
 		vim.cmd.e(filename)
-		helpers.compile({ args = "echo %" })
-		assert.are.same({ "echo " .. filename, filename }, helpers.get_output())
+		helpers.compile({ args = print_command("%") })
+		assert.are.same({ print_command(filename), filename }, helpers.get_output())
 	end)
 
 	it("should escape properly", function()
 		vim.cmd.e(filename)
-		helpers.compile({ args = "echo \\%" })
-		assert.are.same({ "echo %", "%" }, helpers.get_output())
+		helpers.compile({ args = print_command("\\%") })
+		assert.are.same({ print_command("%"), "%" }, helpers.get_output())
 
-		helpers.compile({ args = "echo \\\\\\\\\\%" })
-		assert.are.same({ "echo \\\\%", "\\%" }, helpers.get_output())
+		helpers.compile({ args = print_command("\\\\\\%") })
+		assert.are.same({ print_command("\\%"), "\\%" }, helpers.get_output())
 	end)
 end)
